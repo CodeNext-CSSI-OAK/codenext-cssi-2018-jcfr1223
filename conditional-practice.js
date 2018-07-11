adbc7771eec02dff60fd164c55cd896f637fa673
Student <student@Admins-MacBook-Air.local>
2018-07-11 16:17:51 -0700

    flowchart

---
+++ conditional-practice.js
@@ -0,0 +1,13 @@
+let readline = require("readline-sync");
+let ourNum = 5;
+let userNum = Number(readline.question("Please guess a number from 1 to 10: "));
+
+if (userNum === ourNum) {
+  console.log("You guessed it! Lucky!");
+}else if (userNum > ourNum){
+  console.log("You guessed too high");
+} else {
+  console.log("You guessed too low")
+}
+
+//Math.floor(Math.random())
